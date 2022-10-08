3e93bf8527ec5f5b1e851c7ad4cf7a4636e3e26b
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 10:12:34 +0300

    write a function that adds new contacts to list of contacts

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,12 +1,97 @@
-import react from "react";
+import  { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 
-function AddContact(){
+function AddContact(addedContact, handleNewContact) {
+  const history = useHistory();
+  const [newContact, setNewContact] = useState({
+    firstname: "",
+    lastname: "",
+    phone: "",
+    email: "",
+  });
 
+  function handleChange(e) {
+    // console.log(e.target.value);
+    const name = e.target.name;
+    console.log(e.target.name)
+    let value = e.target.value;
+    setNewContact({ ...newContact, [name]: value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/contacts',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(newContact)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+    })
+    handleNewContact()
+    history.push("/")
+}
 
-    return(
-        <div>
-            <p>Create contact test</p>
-        </div>
-    )
-} 
-export default AddContact
+if (!addedContact) 
+return <Redirect to="/" />;
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit} className="contacts">
+          <label>
+            First Name :{" "}
+            <input
+              onChange={handleChange}
+              className="formInput"
+              type="text"
+              placeholder="Enter first name"
+              name="firstname"
+            />
+          </label>
+
+          <label>
+            Second Name :{" "}
+            <input
+              onChange={handleChange}
+              className="formInput"
+              type="text"
+              placeholder="Enter last name"
+              name="lastname"
+            />
+          </label>
+
+          <label>
+            Phone :{" "}
+            <input
+              onChange={handleChange}
+              className="formInput"
+              type="text"
+              placeholder="Enter phone number"
+              name="phone"
+            />
+          </label>
+
+          <label>
+            Email :{" "}
+            <input
+              onChange={handleChange}
+              className="formInput"
+              type="text"
+              placeholder="Enter email"
+              name="email"
+            />
+          </label>
+          <input
+            // style={{ width: "100px", marginTop: "30px", marginLeft: "280px" }}
+            type="submit"
+            value="Add to Contacts"
+          />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default AddContact;
