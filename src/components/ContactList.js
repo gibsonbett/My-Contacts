686d78ba1a5bd24d70cd03fdff318ef70cfe6aaa
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 10:15:15 +0300

    filter data for search and mapped through contacts to find searched data

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Contact from "./Contact";
 
+function ContactList({ contacts, search }) {
+const filterData=contacts.filter((everyContact)=>
+    everyContact.firstname.toLowerCase().includes(search.toLowerCase())
+)
 
-function ContactList({contacts, setContacts}){
-console.log (contacts)
-
-
-    return(
-        <div>
-               All contacts
-            {contacts.map((contact) => (
-                <Contact 
-                key = {contact.id}
-                firstname = {contact.firstname} 
-                lastname = {contact.lastname} 
-                phone = {contact.phone} 
-                email = {contact.email} 
-                />
-            ))}
-        </div>
-    )
+  return (
+    <div style={{border: '1px solid red'}}>
+      All contacts
+      {filterData.map((contact) => (
+        <Contact
+          key={contact.id}
+          firstname={contact.firstname}
+          lastname={contact.lastname}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
+    </div>
+  );
 }
-export default ContactList
+export default ContactList;
