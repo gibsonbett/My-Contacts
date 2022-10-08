87525455970b70fd52dbca42e93ba4fe5f2e01b8
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 14:25:47 +0300

    add button to delete saved contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-function Contact({ firstname, lastname, phone, email}) {
-  console.log(firstname)
+function Contact({ firstname, lastname, phone, email, id, deleteContact }) {
+  console.log(firstname);
   return (
-  <div style={{border: '1px solid blue'}}>
-    <tr>
-      <td>{firstname}</td>
-      <td>{lastname}</td>
-      <td>{phone}</td>
-      <td>{email}</td>
-    </tr>
-  </div>
-
+    <div className="contact">
+      <th>
+        <td>{firstname}</td>
+        <td>{lastname}</td>
+        <td>{phone}</td>
+        <td>{email}</td>
+        <hr />
+      </th>
+      <div id="delete">
+          <button id="btn" onClick={() => deleteContact(id)}>
+            delete
+          </button>
+        </div>
+    </div>
   );
 }
 
