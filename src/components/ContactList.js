2a14564779c6c3bd6497d28dd134daab83d532b9
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 14:26:35 +0300

    add a function that deletes contacts when called by delete button and updates new contacts

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Contact from "./Contact";
 
-function ContactList({ contacts, search }) {
+function ContactList({ contacts, search, setContacts }) {
 const filterData=contacts.filter((everyContact)=>
     everyContact.firstname.toLowerCase().includes(search.toLowerCase())
 )
+
+function deleteContact(id) {
+  fetch(`http://localhost:3000/contacts/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then(() => {
+      const updatedContacts = contacts.filter(
+        (remainingContacts) => remainingContacts.id !== id
+      );
+      setContacts(updatedContacts);
+    });
+}
+
 
   return (
     <div style={{border: '1px solid red'}}>
       All contacts
       {filterData.map((contact) => (
         <Contact
-          key={contact.id}
+          deleteContact={deleteContact}
+          id={contact.id}
           firstname={contact.firstname}
           lastname={contact.lastname}
           phone={contact.phone}
