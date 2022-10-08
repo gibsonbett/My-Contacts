8685a60cd2a6407daa24d7a0a2f0d65ba6876e8a
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 10:14:25 +0300

    give the returned elements a div for styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 function Contact({ firstname, lastname, phone, email}) {
+  console.log(firstname)
   return (
+  <div style={{border: '1px solid blue'}}>
     <tr>
       <td>{firstname}</td>
       <td>{lastname}</td>
       <td>{phone}</td>
       <td>{email}</td>
     </tr>
+  </div>
+
   );
 }
 
