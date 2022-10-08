4808c2216e1495bbd877a9204d622ec170c33f00
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 14:23:46 +0300

    import components and added to react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
+import react from 'react';
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Search from './Search';
 import ContactsPage from './ContactsPage';
+import ContactList from './ContactList';
+import AddContact from './AddContact';
 
 function App() {
 
-  return (
-  <div className=''>
-          <p> App test</p>
-      <ContactsPage/>
+  return <div className=''>
+    <NavBar/>
+      <Switch>
+        <Route exact path="/contacts">
+          <ContactsPage />
+        </Route>
+        <Route exact path="/addcontact">
+          <AddContact/>
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
   </div>
-  )
+  
 }
 
 export default App;
