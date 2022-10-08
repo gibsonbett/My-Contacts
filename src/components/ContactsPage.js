3f0206233088e13abbb3a94ff4c118c0d9c039b6
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-08 10:17:59 +0300

    imported search and addcontact components, fetch data from json server handle new contact and searched contacts

--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from "react";
 import ContactList from "./ContactList";
-
+import Search from "./Search";
+import AddContact from "./AddContact";
 function ContactsPage() {
   // fetch contacts from json
   const [contacts, setContacts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [addedContact, setAddedContact] = useState(false)
+  // const [isAdded, setIsAdded] = useState(false)
 
   useEffect(() => {
-       fetch("http://localhost:3000/contacts")
+    fetch("http://localhost:3000/contacts")
       .then((res) => res.json())
-      .then((data) => setContacts(data))
-
-  }, []);
-
-  console.log(contacts)
+      .then((data) => setContacts(data));
+  }, [addedContact]);
+     
+  function handleNewContact(){
+    setAddedContact((addedContact)=>!addedContact )
+  }
+  // console.log(contacts);
 
   return (
-    <div className="App">
-      <ContactList contacts={contacts} setContacts={setContacts} />
+    <div className="">
+      <div>
+        <Search 
+        setSearch = {setSearch}
+        />
+        <AddContact setAddedContact = {handleNewContact}/>
+      </div>
+      <ContactList 
+      
+      search= {search}
+      contacts={contacts} 
+      setContacts={setContacts} />
     </div>
   );
 }
