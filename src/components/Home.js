7cb89d6a59db7f369f0b0d1737c140fb2eb4e4aa
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-09 14:40:22 +0300

    add elements to component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div><h1>Home Page</h1></div>;
+  return (
+    <div id="home">
+      <h1>Welcome to Contacts</h1>
+      <h3>Manage your contacts, anywhere you are.</h3>
+    </div>
+  );
 }
 
 export default Home;
