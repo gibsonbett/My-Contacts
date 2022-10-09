ba97b987fc2c704fa482676313277638b034c4a6
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-09 14:39:39 +0300

    fixed bugs

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,28 +1,35 @@
 import React from "react";
 import Contact from "./Contact";
 
-function ContactList({ contacts, search, setContacts }) {
-const filterData=contacts.filter((everyContact)=>
+function ContactList({ contacts, search, setContacts, setSearch }) {
+  console.log(contacts)
+  const filterData = contacts.filter((everyContact) =>
     everyContact.firstname.toLowerCase().includes(search.toLowerCase())
-)
+  );
 
-function deleteContact(id) {
-  fetch(`http://localhost:3000/contacts/${id}`, {
-    method: "DELETE",
-  })
-    .then((res) => res.json())
-    .then(() => {
-      const updatedContacts = contacts.filter(
-        (remainingContacts) => remainingContacts.id !== id
-      );
-      setContacts(updatedContacts);
-    });
-}
+  function deleteContact(id) {
+    fetch(`http://localhost:3000/contacts/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        const updatedContacts = contacts.filter(
+          (remainingContacts) => remainingContacts.id !== id
+        );
+        setContacts(updatedContacts);
+      });
+  }
 
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
 
   return (
-    <div style={{border: '1px solid red'}}>
-      All contacts
+    <div className="contactlist">
+      ALL CONTACTS
+      <input id="search" type="text" placeholder="Search Contact" onChange={handleSearch} />
+      <i className=""></i>
+      
       {filterData.map((contact) => (
         <Contact
           deleteContact={deleteContact}
