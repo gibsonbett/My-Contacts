9015587b7a03406b1355e1b9a54ab993f6f862b0
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-09 20:59:45 +0300

    delete redundant code

--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ContactList from "./ContactList";
 function ContactsPage({contacts, search, setContacts, setSearch}) {
 
