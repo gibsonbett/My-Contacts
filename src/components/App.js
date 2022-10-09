4f89a917b7bd9470e0735ea4cef102528745a9cb
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-09 14:39:04 +0300

    write function that fetch data from server

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,47 @@
-import react from 'react';
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import Search from './Search';
-import ContactsPage from './ContactsPage';
-import ContactList from './ContactList';
-import AddContact from './AddContact';
+import ContactsPage from "./ContactsPage";
+import AddContact from "./AddContact";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [newContact, setNewContact] = useState(false);
 
-  return <div className=''>
-    <NavBar/>
+  useEffect(() => {
+    fetch("http://localhost:3000/contacts")
+      .then((res) => res.json())
+      .then((data) => {
+        setContacts(data);
+      });
+  }, [newContact]);
+  function handleNewContact() {
+    setNewContact((newContact) => newContact);
+  }
+
+  return (
+    <div className="">
+      <NavBar />
       <Switch>
-        <Route exact path="/contacts">
-          <ContactsPage />
-        </Route>
         <Route exact path="/addcontact">
-          <AddContact/>
+          <AddContact handleNewContact={handleNewContact} />
         </Route>
-        <Route exact path="/search">
-          <Search />
+        <Route exact path="/contacts">
+          <ContactsPage
+            contacts={contacts}
+            search={search}
+            setContacts={setContacts}
+            setSearch={setSearch}
+          />
         </Route>
         <Route exact path="/">
           <Home />
         </Route>
       </Switch>
-  </div>
-  
+    </div>
+  );
 }
 
 export default App;
