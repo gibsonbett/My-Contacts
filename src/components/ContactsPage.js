7d24270a9f6765bbc4733d3623c3ce3e89dbc113
gibsonbett <gibson.bett@student.moringaschool.com>
2022-10-09 14:41:05 +0300

    delete fetch function from component

--- src/components/ContactsPage.js
+++ src/components/ContactsPage.js
@@ -1,38 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ContactList from "./ContactList";
-import Search from "./Search";
-import AddContact from "./AddContact";
-function ContactsPage() {
-
-  const [contacts, setContacts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [addedContact, setAddedContact] = useState(false)
-
-  useEffect(() => {
-    fetch("http://localhost:3000/contacts")
-      .then((res) => res.json())
-      .then((data) => {setContacts(data)});
-  }, [addedContact]);
-     
-  function handleNewContact(){
-    setAddedContact((addedContact)=>!addedContact)
-  }
-  // console.log(contacts);
+function ContactsPage({contacts, search, setContacts, setSearch}) {
 
   return (
-    <div className="contactList">
-      <div>
-        <Search 
-        setSearch = {setSearch}
-        />
-        <AddContact setAddedContact = {handleNewContact}/>
-      </div>
-      <ContactList 
-      
-      search= {search}
-      contacts={contacts} 
-      setContacts={setContacts} />
+    <div>
+<ContactList 
+  contacts={contacts} 
+  search={search} 
+  setContacts={setContacts} 
+  setSearch={setSearch}
+  />
     </div>
-  );
+  )
 }
 export default ContactsPage;
